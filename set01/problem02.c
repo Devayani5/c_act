889e0ad637d4ca67221f47b1babadcf4c6e0f3e7
Devayani5 <141308808+Devayani5@users.noreply.github.com>
2024-01-23 09:57:11 +0530

    test

--- set01/problem02.c
+++ set01/problem02.c
@@ -1,2 +1,13 @@
 //2. Write a C program to add two numbers.
-
+ 
+#include<stdio.h>
+int main()
+{
+    int num1,num2;
+    printf("enter the first number:");
+    scanf("%d"&num1);
+    printf("enteret he second number:");
+    scanf("%d",&num2);
+    printf("the sum of %d is %d \n",num 1,num 2,sum);
+    return 0;
+}
